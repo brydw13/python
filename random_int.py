20f331d3ffa7ad612971df19121486a507cf7668
Bryan <31550939+brydw13@users.noreply.github.com>
2017-10-30 13:46:03 -0700

    Add files via upload

--- random_int.py
+++ random_int.py
@@ -2,24 +2,24 @@
 from random import randint
 def randlist(r, usedlist, done):
 	sum = 0
-	alpha = ["a","A","b","B,","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","!","@","#","$","%","^","&","*","(",")","-","_","=","+",":","|","\\"]
+	alpha = ['!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
 	usedlist[r] = 1
 	c = alpha[r]
 	print (len(usedlist))
 	for i in range (len(usedlist)):
 		sum = sum + usedlist[i]
 	#print (c, usedlist, "sum", sum)
-	if sum == 6:
+	if sum == 94:
 		done = True
 	return c, usedlist, done 
 	
 def main():
 	# initial variables
-	used = [0,0,0,0,0,0]
+	used = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	done = False #boolean
 	######################
 	while done == False:
-		r = randint(0,5) # make a random number
+		r = randint(0,93) # make a random number
 		c,used,done = randlist(r, used, done)
 		#print (used,)
 		print(c,end="")

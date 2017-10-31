25b881f3a2c6c3c4f4dbfcaa7a73191db0ed03a6
Bryan <31550939+brydw13@users.noreply.github.com>
2017-10-31 13:51:47 -0700

    Add files via upload

--- count.py
+++ count.py
@@ -1,6 +1,7 @@
 count = 0
 while True:
-	print(count, "=" ,bin(count))
+	print(count,"=",bin(count))
 	count = count + 1
-	if count >100: break
+	if count > 100: break
 print("count reached 100", count)
+  
